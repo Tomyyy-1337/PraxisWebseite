478b0a4e575a6e68dedead3ed842b8955dd15cc6
Tom Paßberg <passberg@rhrk.uni-kl.de>
2023-09-20 22:49:32 +0200

    -

--- Project/Content/urls.py
+++ Project/Content/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("Leistungen", views.leistungen, name="leistungen"),
+    path("Kontakt", views.kontakt, name="kontakt"),
+    path("Impressum", views.impressum, name="impressum"),
 ]
--- Project/Content/views.py
+++ Project/Content/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "Content/home.html")
+
+def leistungen(request):
+    return render(request, "Content/leistungen.html")
+
+def kontakt(request):
+    return render(request, "Content/kontakt.html")
+
+def impressum(request):
+    return render(request, "Content/impressum.html")
