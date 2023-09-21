01eb365958f785a6b91972c7a02e7fefa8bea50b
Tom Paßberg <passberg@rhrk.uni-kl.de>
2023-09-21 22:29:26 +0200

    donnerstag

--- Project/Content/views.py
+++ Project/Content/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.core.mail import send_mail
 
 def home(request):
-    return render(request, "Content/home.html")
+    if(request.POST.get("Name","") and request.POST.get("Email","") and request.POST.get("Nachricht","")):
+        Data = {"nachrichtGesendet": True}
+        print(request.POST.get("Nachricht",""))
+    else:
+        Data = {"nachrichtGesendet": False}
+    return render(request, "Content/home.html", Data)
 
 def leistungen(request):
     return render(request, "Content/leistungen.html")
